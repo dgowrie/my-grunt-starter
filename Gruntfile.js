063f9d906283625657f0660ea8b232035262c023
David Gowrie <david.gowrie@gmail.com>
2013-12-05 02:38:08 -0500

    Overhaul of Grunt configuration - package.json build with updated devDeps - Gruntfile.js rebuilt with project setup and task configuration - using livereload, local webserver

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,64 @@
+'use strict';
+
+/**
+ * Livereload and connect variables
+ */
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({
+	port: LIVERELOAD_PORT
+});
+var mountFolder = function(connect, dir) {
+	return connect.static(require('path').resolve(dir));
+};
+
+/**
+ * Grunt module
+ */
 module.exports = function(grunt) {
+
+	/**
+	 * Dynamically load npm tasks
+	 */
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 	
-	// Project configuration.
+	/**
+	 * Grunt config
+	 */
 	grunt.initConfig({
 	
-		// Metadata.
+		//
+		// Meta
+		//
+
 		pkg: grunt.file.readJSON('package.json'),
-		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-		'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-		'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-		'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-		' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-		
-		// Task configuration.
-		concat: {
+
+		/**
+		 * Set project info
+		 */
+		project: {
+			src: 'src',
+			assets: '<%= project.src %>/',
+			scss: '<%= project.src %>/styles/sass/style.scss',
+			js: ['<%= project.src %>/js/'],
+			build: 'build'
+		},
+
+		/**
+		 * Project banner
+		 * Dynamically appended to styles/JS files
+		 * Inherits text from package.json
+		 */
+		banner: '/*!\n' +
+		' * <%= pkg.name %>\n' + 
+		' * <%= pkg.title %>\n' +
+		' * <%= pkg.url %>\n' + 
+		' * @author <%= pkg.author %>\n' + 
+		' * @version <%= pkg.version %>\n' + 
+		' * Copyright (c) <%= grunt.template.today("yyyy") %>\n' +
+		' */\n',
+
+		// Original Task configuration.
+/*		concat: {
 			options: {
 				banner: '<%= banner %>',
 				stripBanners: true
@@ -30,51 +76,222 @@ module.exports = function(grunt) {
 				src: '<%= concat.dist.dest %>',
 				dest: 'dist/<%= pkg.name %>.min.js'
 			}
-		},
-		jshint: {
+		}
+*/	
+
+		//
+		// Server Related Tasks
+		//
+
+		/**
+		 * Connect port/livereload
+		 * https://github.com/gruntjs/grunt-contrib-connect
+		 * Starts a local webserver and injects
+		 * livereload snippet
+		 */
+		connect: {
 			options: {
-				curly: true,
-				eqeqeq: true,
-				immed: true,
-				latedef: true,
-				newcap: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				unused: true,
-				boss: true,
-				eqnull: true,
-				browser: true,
-				globals: {}
+				port: 9000,
+				hostname: '*'
 			},
-			gruntfile: {
-				src: 'Gruntfile.js'
-			},
-			lib_test: {
-				src: ['lib/**/*.js', 'test/**/*.js']
+			livereload: {
+				options: {
+					middleware: function(connect) {
+						return [lrSnippet, mountFolder(connect, 'src')];
+					}
+				}
 			}
 		},
-		watch: {
-			gruntfile: {
-				files: '<%= jshint.gruntfile.src %>',
-				tasks: ['jshint:gruntfile']
+
+		/**
+		 * Opens the web server in the browser
+		 * https://github.com/jsoverson/grunt-open
+		 */
+		open: {
+			server: {
+				path: 'http://localhost:<%= connect.options.port %>',
+				app: 'Google Chrome'
+			}
+		},
+
+
+		//
+		// JS Tasks
+		//
+
+		/**
+		 * JSHint
+		 * https://github.com/gruntjs/grunt-contrib-jshint
+		 * Manage the options inside .jshintrc file
+		 */
+		jshint: {
+			files: ['<%= project.js %>script.js', 'Gruntfile.js'],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		/**
+		 * Concatenate JavaScript files
+		 * https://github.com/gruntjs/grunt-contrib-concat
+		 * Imports all .js files and appends project banner
+		 */
+		concat: {
+			dev: {
+				files: {
+					'<%= project.js %>scripts.min.js': ['<%= project.js %>script.js']
+				}
 			},
-			lib_test: {
-				files: '<%= jshint.lib_test.src %>',
-				tasks: ['jshint:lib_test', 'qunit']
+			options: {
+				stripBanners: true,
+				nonull: true,
+				banner: '<%= banner %>'
+			}
+		},
+
+		/**
+		 * Uglify (minify) JavaScript files
+		 * https://github.com/gruntjs/grunt-contrib-uglify
+		 * Compresses and minifies all JavaScript files into one
+		 */
+		uglify: {
+			options: {
+				preserveComments: false,
+				banner: '<%= banner %>'
+			},
+			build: {
+				files: {
+				'<%= project.build %>/js/scripts.min.js': '<%= project.js %>scripts.min.js'
+				}
+			}
+		},
+
+
+		//
+		// CSS Tasks
+		//
+
+		/**
+		 * Compile Sass/SCSS files
+		 * https://github.com/gruntjs/grunt-contrib-sass
+		 * Compiles all Sass/SCSS files and appends project banner
+		 */
+		sass: {
+			dev: {
+				options: {
+					style: 'expanded',
+					banner: '<%= banner %>',
+					lineNumbers: true
+				},
+				files: {
+					'<%= project.assets %>styles/style.unprefixed.css': '<%= project.scss %>'
+				}
+			}
+		},
+
+		/**
+		 * Autoprefixer
+		 * Adds vendor prefixes automatically
+		 * https://github.com/nDmitry/grunt-autoprefixer
+		 */
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 version', 'safari 6', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+			},
+			dev: {
+				files: {
+					'<%= project.assets %>styles/style.min.css': ['<%= project.assets %>styles/style.unprefixed.css']
+				}
+			}
+		},
+
+		/**
+		 * CSSMin
+		 * CSS minification
+		 * https://github.com/gruntjs/grunt-contrib-cssmin
+		 */
+		cssmin: {
+			build: {
+				options: {
+					banner: '<%= banner %>'
+				},
+				files: {
+					'<%= project.build %>/styles/style.min.css': ['<%= project.assets %>styles/style.min.css']
+				}
+			}
+		},
+
+
+		//
+		// Housekeeping
+		//
+
+		/**
+		 * Copies files to the build dir
+		 * https://github.com/gruntjs/grunt-contrib-copy
+		 */
+		copy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= project.assets %>',
+						src: ['*', 'images/*'],
+						dest: '<%= project.build %>/'
+					}
+				]
+			}
+		},
+
+		/**
+		 * Clean files and folders
+		 * https://github.com/gruntjs/grunt-contrib-clean
+		 * Remove generated files for clean deploy
+		 */
+		clean: {
+			build: ['<%= project.assets %>styles/style.unprefixed.css', '<%= project.assets %>styles/style.prefixed.css']
+		},
+
+		/**
+		 * Runs tasks against changed watched files
+		 * https://github.com/gruntjs/grunt-contrib-watch
+		 * Watching development files and run concat/compile tasks
+		 * Livereload the browser once complete
+		 */
+		watch: {
+			concat: {
+				files: ['<%= project.src %>/js/{,*/}*.js'],
+				tasks: ['concat:dev']
+			},
+			sass: {
+				files: ['<%= project.src %>/styles/sass/{,*/}*.{scss,sass}'],
+				tasks: ['sass:dev', 'autoprefixer:dev']
+			},
+			livereload: {
+				options: {
+					livereload: LIVERELOAD_PORT
+				},
+				files: ['<%= project.src %>/{,*/}*.html', '<%= project.assets %>styles/*.css', '<%= project.assets %>js/{,*/}scripts.min.js', '<%= project.assets %>{,*/}*.{png,jpg,jpeg,gif,webp,svg}']
 			}
 		}
+
 	});
 	
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin'); // redundant (mostly), sass
-	grunt.loadNpmTasks('grunt-contrib-concat'); // redundant? uglifyJS
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 	
-	// Default task.
-	grunt.registerTask('default', ['sass', 'uglify', 'jshint']);
+	//
+	// Tasks Registry
+	//
 
+	/**
+	 * Default task
+	 * Run `grunt` on the command line
+	 */
+	grunt.registerTask('default', ['sass:dev', 'autoprefixer:dev', 'concat:dev', 'connect:livereload', 'open', 'watch']);
+
+	/**
+	 * Build task
+	 * Run `grunt build` on the command line
+	 * Then compress all JS/CSS files
+	 */
+	grunt.registerTask('build', ['sass:dev', 'autoprefixer:dev', 'cssmin:build', 'concat:dev', 'jshint', 'uglify', 'copy:build']);
 };
